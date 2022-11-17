9fd362453c8c10abebfd0fe18d7f1fc7e7ccd121
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-11-17 00:39:32 -0300

    findSales for Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -14,8 +18,17 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() { // definindo uma lista de Sale no return
-		return repository.findAll(); // findAll do JPA
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) { // definindo uma page de Sale no return
+		
+		// Dia atual
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		// se nao for informada vira o ano passado
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		// if ternario se a data max for vazia vira o dia atual
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable); 
 	}
 	
 }
