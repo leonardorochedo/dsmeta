9cf43e8919193fa41a277725d1710e26efd37875
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-11-18 15:41:23 -0300

    Send Notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,25 @@
-import icon from '../../assets/img/notification-icon.svg';
+import icon from "../../assets/img/notification-icon.svg";
 
-import './styles.css';
+import { BASE_URL } from "../../utils/request";
 
-export function NotificationButton() {
-    return (
-        <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notificar" />
-        </div>
-    )
+import "./styles.css";
+
+import axios from "axios";
+
+type Props = {
+  saleId: number;
+};
+
+export function NotificationButton({ saleId }: Props) {
+  function handleClick(id: number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification`).then(() => {
+      console.log("Sucesso");
+    });
+  }
+
+  return (
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+      <img src={icon} alt="Notificar" />
+    </div>
+  );
 }
